5b47b6dcbc77ce64f80c34c998ad5326a42bd2d8
Aayush Garg <aayushgargiitr@gmail.com>
2025-10-05 15:42:18 +0530

    BPE Optimization: Train Custom BPE with different data and vocab size, compare compression ratios

--- bpe/train_TinyStoriesV2.py
+++ bpe/train_TinyStoriesV2.py
@@ -1,13 +1,15 @@
-import os
 
 from bpe import BpeTokenizer
 
 if __name__ == '__main__':
-    vocab_size = 2048
-    input_file_path = "data/TinyStoriesV2-GPT4-train.txt"
+    # vocab_size = 2048
+    # vocab_size = 8192
+    vocab_size = 16384
+    # input_file_path = "data/TinyStoriesV2-GPT4-train.txt"
+    input_file_path = "data/000_00003.txt"
     special_tokens = ["<|endoftext|>"]
     boundary_split_token = special_tokens[0]
-    num_processes = os.cpu_count() // 2
+    num_processes = 10 #os.cpu_count() 
 
     tokenizer = BpeTokenizer()
 
@@ -17,5 +19,5 @@ if __name__ == '__main__':
                     num_processes=num_processes, verbose=True
                     )
     tokenizer.register_special_tokens({"<|endoftext|>": vocab_size})
-    tokenizer.save(f"data/tinystoriesv2-gpt4-train-{vocab_size}")
-    tokenizer.save(f"data/tinystoriesv2-gpt4-train-{vocab_size}")
+    tokenizer.save(f"data/fineweb-000_00003-gpt4-train-{vocab_size}")
+    tokenizer.save(f"data/fineweb-000_00003-gpt4-train-{vocab_size}")
